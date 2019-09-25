88640f2ca3c2589a7fdee0a46cb777b8b4ae296a
medjitov10 <medjitov10@gmail.com>
2019-09-25 13:40:37 -0400

    sign up request

---
+++ src/sign-up.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded');
+    const form = document.querySelector('#signup');
+    form.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('in form');
+        const email = document.querySelector('input[name="email"]').value;
+        const username = email.split('@')[0];
+        const password = document.querySelector('input[name="password"]').value;
+        console.log(typeof(email));
+
+        userObj = {
+            "email": email, 
+            'password': password, 
+            'username': username
+        }
+        
+        fetch('http://thesi.generalassemb.ly:8080/signup', {
+            method: 'POST',
+            body: JSON.stringify(userObj),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);    
+            sessionStorage.setItem('token', data.token);
+            sessionStorage.setItem('username', username);
+            window.location.href = '/'
+        })
+        .catch(err => {
+            console.log(err);
+            
+        })
+        // fetch('http://thesi.generalassemb.ly:8080/post/list').then(el => el.json()).then(el=> {
+        //     console.log(el)
+        // })
+    })
+})
